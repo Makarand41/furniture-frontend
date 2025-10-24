a170447fc3734076b9f94f85121eb9c80fd1b87c
Makarand41 <makarandpatil67@gmail.com>
2025-10-24 10:26:36 +0530

    Added Vercel Analytics integration

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 
 import App from './App.jsx'
+import { inject } from "@vercel/analytics";
+
+inject(); // enable analytics
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
